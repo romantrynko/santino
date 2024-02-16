d4578bff28a78f5c74d9eee4f022806a240e5f0b
Nazar Trynko <nazar.trynko@empeek.io>
2024-02-16 15:56:51 +0200

    added list of products

--- src/app/pageContent/HomePageContent/HomePageContent.tsx
+++ src/app/pageContent/HomePageContent/HomePageContent.tsx
@@ -6,28 +6,26 @@ import { useRouter } from 'next/navigation';
 import React, { useMemo } from 'react';
 
 const buttons = [
-  { value: 1, name: 'Breakfast', path: '/menu/breakfast' },
-  { value: 2, name: 'Lunch', path: '/menu/lunch' },
-  { value: 3, name: 'Dinner', path: '/menu/dinner' },
-  { value: 4, name: 'Dessert', path: '/menu/dessert' },
-  { value: 5, name: 'Drinks', path: '/menu/drinks' },
-  { value: 6, name: 'Specials', path: 'menu/specials' },
-  { value: 7, name: 'Contact', path: '/menu/contact' }
+  { value: 1, name: 'Breakfast', category: 'breakfast' },
+  { value: 2, name: 'Lunch', category: 'lunch' },
+  { value: 3, name: 'Dinner', category: 'dinner' },
+  { value: 4, name: 'Dessert', category: 'dessert' },
+  { value: 5, name: 'Drinks', category: 'drinks' },
+  { value: 6, name: 'Specials', category: 'specials' },
+  { value: 7, name: 'Contact', category: 'contact' }
 ];
 
 const HomePageContent = () => {
   const { isMobileInPortrait } = useScreenSize();
 
-
   const router = useRouter();
 
-  const handleButtonClick = useMemo(() => (route: string) => {
+  const handleButtonClick = useMemo(() => (category: string) => {
     setTimeout(() => {
-      router.push(route);
+      router.push(`/${category}`);
     }, 300);
     
   }, [router]);
-
 
   return (
     <div
@@ -40,12 +38,12 @@ const HomePageContent = () => {
         textClassName="font-extralight text-white text-[70px] tracking-widest"
       />
       <div className={cn('flex flex-col w-full h-full gap-2 mt-2', {})}>
-        {buttons.map(({ name, path }) => (
+        {buttons.map(({ name, category }) => (
           <MenuSectionButton
             key={name}
             containerClassName="bg-gray-500 p-4 rounded-full transition duration-300 ease-in-out cursor-pointer active:bg-gray-900 active:transform active:translate-1 active:duration-300 active:ease-in-out"
             textClassName="text-[20px] font-serif font-bold "
-            onClick={() => handleButtonClick(path)}
+            onClick={() => handleButtonClick(category)}
             >
             {name}
            
